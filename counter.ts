b0c0f4ffca89278357da04689a8c0c88bcb371b6
WangMaoquan <1782976211@qq.com>
2021-09-03 15:04:58 +0800

    2 files modified

--- counter.ts
+++ counter.ts
@@ -1,10 +1,47 @@
+import { filter, fromEvent, Subject } from 'rxjs';
+
 // 获取按钮
-const oStart = document.querySelector('.start');
-const oCount = document.querySelector('.count');
-const oError = document.querySelector('.error');
-const oComplete = document.querySelector('.complete');
+const oStartBtn = document.querySelector('.start');
+const oCountBtn = document.querySelector('.count');
+const oErrorBtn = document.querySelector('.error');
+const oCompleteBtn = document.querySelector('.complete');
 
 // 获取显示区
 const oCurrentStatus = document.querySelector('.current_status');
 const oCurrentCount = document.querySelector('.current_count');
 const oEvenCount = document.querySelector('.even_count');
+
+// 计数器的值
+let count = 0;
+// 可被观察的对象习惯在结尾加一个$
+let count$: Subject<number>;
+
+fromEvent(oStartBtn, 'click').subscribe(() => {
+  // 初始化
+  count$ = new Subject();
+  count = 0;
+  oCurrentStatus.innerHTML = '当前状态: 开始计数';
+  // 分离出去
+  // count$.subscribe(data => {
+  //   oCurrentCount.innerHTML = `目前计数: ${data}`;
+  //   if (data % 2 === 0) {
+  //     oEvenCount.innerHTML = `偶数计数: ${data}`;
+  //   }
+  // });
+  count$.next(count);
+});
+
+// 专注于当前计数
+count$.subscribe(data => {
+  oCurrentCount.innerHTML = `目前计数: ${data}`;
+});
+
+// 专注于偶数计数
+const evenCount$ = count$.pipe(filter(data => data % 2 === 0));
+evenCount$.subscribe(data => {
+  oEvenCount.innerHTML = `偶数计数: ${data}`;
+});
+
+fromEvent(oCountBtn, 'click').subscribe(() => {
+  count$.next(++count);
+});
--- index.ts
+++ index.ts
@@ -2,20 +2,20 @@ import './style.css';
 import './counter';
 import { of, map, filter, fromEvent, Subject } from 'rxjs';
 
-fromEvent(document, 'click')
-  .pipe(
-    filter((e, index) => {
-      console.log(e);
-      return index % 2 === 0;
-    }),
-    map(e => {
-      const { x, y } = e as MouseEvent;
-      return {
-        x,
-        y
-      };
-    })
-  )
-  .subscribe(position => {
-    console.log(position);
-  });
+// fromEvent(document, 'click')
+//   .pipe(
+//     filter((e, index) => {
+//       console.log(e);
+//       return index % 2 === 0;
+//     }),
+//     map(e => {
+//       const { x, y } = e as MouseEvent;
+//       return {
+//         x,
+//         y
+//       };
+//     })
+//   )
+//   .subscribe(position => {
+//     console.log(position);
+//   });
