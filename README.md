ae1b42038cb62f5dc564ad0b05893b0f386fde80
WangMaoquan <1782976211@qq.com>
2021-09-03 10:25:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-hftmti
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-hftmti)
---
+++ counter.ts
@@ -0,0 +1,10 @@
+// 获取按钮
+const oStart = document.querySelector('.start');
+const oCount = document.querySelector('.count');
+const oError = document.querySelector('.error');
+const oComplete = document.querySelector('.complete');
+
+// 获取显示区
+const oCurrentStatus = document.querySelector('.current_status');
+const oCurrentCount = document.querySelector('.current_count');
+const oEvenCount = document.querySelector('.even_count');
